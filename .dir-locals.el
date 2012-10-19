d1c1354507ad9be02fdabd1f35a3672e550a9221
Thomas Frössman <thomasf@jossystem.se>
2012-10-19 09:58:41 +0200

    added emacs dir locals

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((js-mode
+  (indent-tabs-mode . t))
+ (json-mode
+  (indent-tabs-mode . t))
+ (html-mode
+  (indent-tabs-mode . t)))
